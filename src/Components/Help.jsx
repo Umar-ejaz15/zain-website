baaadfa3da22abadeb0117e9041e6863862993fa
umar ejaz <umar.senpaiiii@gmail.com>
2025-02-15 19:03:13 +0500

    v6

--- src/Components/Help.jsx
+++ src/Components/Help.jsx
@@ -32,20 +32,20 @@ const Help = () => {
         </div>
         <div className="border-b-[1px] border-gray-400/20 my-5 w-1/2"></div>
       </div>{" "}
-      <div className="w-full h-auto mt-10 px-20">
-        <h1 className="text-5xl w-2/3 font-bold">
+      <div className="w-full h-auto mt-10 px-5 md:px-20">
+        <h1 className="text-2xl md:text-5xl w-full md:w-2/3 font-bold">
           I{" "}
           <span className=" bg-gradient-to-b from-[#4474E3] to-[#4E58DF] text-transparent bg-clip-text">
             take on the stress
           </span>{" "}
           of designing, building, and managing your website
         </h1>
-        <p className="text-3xl w-2/3 mt-5">
+        <p className="text-xl md:text-3xl md:w-2/3 mt-5">
           So you can spend more time building your product and working on your
           main business goals.
         </p>
       </div>
-      <div className="cards grid grid-cols-3 gap-5 px-20 mt-20">
+      <div className="cards grid grid-col-1 md:grid-cols-3 gap-5 px-5 md:px-20 mt-20">
         {data.map((item, index) => {
           return (
             <Card
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -13,14 +13,14 @@ const Main = () => {
         />
       </div>
 
-      <h3 className="text-2xl font-medium text-gray-700 ">Hi! I am Zain</h3>
+      <h3 className="text-2xl font-medium text-gray-700 mt-3 mb-2">Hi! I am Zain</h3>
 
-      <div className="flex flex-col text-center text-6xl md:text-8xl font-bold leading-none tracking-tighter ">
+      <div className="flex flex-col  text-center text-4xl md:text-8xl font-bold leading-none tracking-tighter ">
         <h1 style={{ fontFamily: "var(--font2)" }}>Website Designer</h1>
         <h1 style={{ fontFamily: "var(--font2)" }}>& SEO Specialist</h1>
       </div>
 
-      <h3 className="text-2xl text-gray-700">While working remotely</h3>
+      <h3 className="text-2xl text-gray-700 mt-5">While working remotely</h3>
       
       <div className="relative z-10">
         <Button btntext="Currently Available" />
